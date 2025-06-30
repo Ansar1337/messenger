dbc840d897e926ba7e5b27bd48c464cc126b2043
Ansar <ansarshayekin1@gmail.com>
2025-06-30 14:38:00 +0100

    added input event and refactored type to an array

--- src/components/TriangleChecker.vue
+++ src/components/TriangleChecker.vue
@@ -10,31 +10,32 @@ function checkTriangle() {
   const a = angle1.value;
   const b = angle2.value;
   const c = angle3.value;
-  let type = '';
+  let type = [];
 
-  result.value = '';
 
   if (a <= 0 || b <= 0 || c <= 0) {
     result.value = 'Ошибка: углы должны быть положительными';
+    return;
   }
 
   if (a + b + c !== 180) {
     result.value = 'Ошибка: сумма углов должна быть равна 180';
+    return;
   }
 
   if (a === 90 || b === 90 || c === 90) {
-    type += 'прямоугольный '
+    type.push('прямоугольный');
   }
 
   if (a === b || b === c || a === c) {
-    type += 'равнобедренный '
+    type.push('равнобедренный');
   }
 
   if (a === b && b === c) {
-    type += 'равносторонний '
+    type.push('равносторонний');
   }
 
-  result.value = 'Тип треугольника: ' + type;
+  result.value = 'Тип треугольника: ' + type.join(', ');
 
 }
 </script>
@@ -43,11 +44,11 @@ function checkTriangle() {
   <div class="triangle-checker">
     <h2>Определение типа треугольника по углам</h2>
     <div class="inputs">
-      <input v-model.number="angle1" type="number" placeholder="Угол 1"/>
-      <input v-model.number="angle2" type="number" placeholder="Угол 2"/>
-      <input v-model.number="angle3" type="number" placeholder="Угол 3"/>
+      <input @input="checkTriangle" v-model.number="angle1" type="number" placeholder="Угол 1"/>
+      <input @input="checkTriangle" v-model.number="angle2" type="number" placeholder="Угол 2"/>
+      <input @input="checkTriangle" v-model.number="angle3" type="number" placeholder="Угол 3"/>
     </div>
-    <button @click="checkTriangle">Проверить</button>
+    <!--    <button @click="checkTriangle">Проверить</button>-->
     <div class="result">{{ result }}</div>
   </div>
 </template>
