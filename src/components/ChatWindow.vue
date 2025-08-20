78ef35c1c4c68d9818fe838afb434f7a43328946
Ansar <ansarshayekin1@gmail.com>
2025-08-20 17:38:01 +0100

    ChatWindow component refactoring

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -13,49 +13,66 @@ function sendMessage() {
 
 <template>
   <div class="chat-window-container">
-    <ChatMessage></ChatMessage>
-    <div class="input-field-container">
-
+    <div class="bubble-messages">
+      <ChatMessage sender-nickname="Ansar"></ChatMessage>
+    </div>
+    <div class="chat-window-utils">
+      <div class="input-field">
+        <textarea>Введите сообщение</textarea>
+      </div>
       <div class="emoji-btn">
         <button @click="">Emoji</button>
       </div>
       <div class="send-btn">
         <button @click="sendMessage">Send</button>
       </div>
-      <div class="input-field">
-        <input
-            placeholder="Введите сообщение..."
-
-        />
-      </div>
     </div>
   </div>
+
 
 </template>
 
 <style scoped>
 .chat-window-container {
-  display: grid;
-  grid-template-columns: 300px 1fr;
-  grid-template-rows: auto 1fr;
-  grid-template-areas:
-  "input emoji send";
-  height: 100vh;
+  display: flex;
+  gap: 8px;
+  align-items: stretch;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.bubble-messages {
+  flex-grow: 1;
+}
+
+.chat-window-utils {
+  display: flex;
+  align-items: center;
 }
 
 .input-field {
   display: flex;
   align-items: center;
-  grid-area: input;
+  flex-grow: 1;
+}
+
+.input-field textarea {
+  flex-grow: 1;
 }
 
 .emoji-btn {
   display: flex;
-  margin-left: 8px;
+  cursor: pointer;
 }
 
 .send-btn {
   display: flex;
+  cursor: pointer;
+}
+
+.emoji-btn:hover,
+.send-btn:hover {
+  background-color: #152a4c;
 }
 
 
