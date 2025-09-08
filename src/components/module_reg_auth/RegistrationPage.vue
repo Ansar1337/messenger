8e5642c08eb5ffa51ebb16162e45a8af8b8f2980
Ansar <ansarshayekin1@gmail.com>
2025-09-08 15:43:34 +0100

    RegistrationPage logic + FieldSet component.
    - added functional logic to RegistrationPage (validation, mode switching).
    - created reusable FieldSet component for form input grouping.

--- src/components/module_reg_auth/RegistrationPage.vue
+++ src/components/module_reg_auth/RegistrationPage.vue
@@ -1,21 +1,46 @@
 <script setup>
-import {ref} from "vue";
+import {ref, reactive} from "vue";
+import FieldSet from "@/components/module_reg_auth/FieldSet.vue";
 
-const form = ref({
+const form = reactive({
   username: "",
   password: "",
   confirmPassword: ""
 });
 
+// error message
+const error = ref("");
+
 const isRegister = ref(false);
 
+// toggle
 function toggleMode() {
   isRegister.value = !isRegister.value;
+  error.value = "";
+}
+
+// submit form function
+function submit(e) {
+  e.preventDefault();
+  error.value = "";
+  if (isRegister.value) {
+    if (form.password !== form.confirmPassword) {
+      error.value = "Passwords are not matching";
+    } else if (form.password === form.confirmPassword) {
+      alert(`Registration successful! User: ${form.username} `);
+    }
+  } else {
+    if (form.username === "Ansar" && form.password === "1234") {
+      alert("Success!");
+    } else {
+      error.value = "Incorrect username or password";
+    }
+  }
 }
 </script>
 
 <template>
-  <!--  Логотип и название-->
+  <!--  Logo and heading-->
   <div class="auth-container">
     <div class="auth-header">
       <!--      <img src="/assets/logo.svg" alt="Логотип">-->
@@ -23,18 +48,20 @@ function toggleMode() {
     </div>
   </div>
 
-  <!--  Форма-->
+  <!--  Form-->
   <form @submit="submit" class="auth-form">
-    <input
+    <FieldSet
         v-model="form.username"
         type="text"
         placeholder="Логин"
-        required
+        name="login"
+        required="true"
     />
     <input
         v-model="form.password"
         type="password"
         placeholder="Пароль"
+        name="passwd"
         required
     />
     <input
@@ -42,9 +69,14 @@ function toggleMode() {
         v-model="form.confirmPassword"
         type="password"
         placeholder="Подтвердите пароль"
+        name="passwd2"
         required
     />
-    <!--    Кнопки-->
+
+    <!--    Error message-->
+    <p v-if="error" style="color:red">{{ error }}</p>
+
+    <!--    Buttons-->
     <button>
       {{ isRegister ? "Создать" : "Войти" }}
     </button>
@@ -57,5 +89,17 @@ function toggleMode() {
 </template>
 
 <style scoped>
+.auth-container {
+  max-width: 300px;
+  margin: auto;
+  text-align: center;
+}
 
+.auth-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5px;
+  margin-top: 50px;
+}
 </style>
