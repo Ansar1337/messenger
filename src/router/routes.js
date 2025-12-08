44838cd2eb65cffd0b8daeb489c965bbe690d3ce
Ansar <ansarshayekin1@gmail.com>
2025-12-08 15:09:42 +0000

    Refactor router to handle asynchronous operations

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,16 +2,24 @@ import {useUserStore} from "@/store/user.js";
 
 const routes = [
     {
-        path: '/', name: "Chat", component: () => import('@/components/module_chat/Chat.vue'), beforeEnter: () => {
+        path: '/',
+        name: "Chat",
+        component: () => import('@/components/module_chat/Chat.vue'),
+        beforeEnter: async () => {
             const userStore = useUserStore();
+            await userStore.loadUserData();
             if (userStore.isLogged === false) {
-                return '/registration';
+                return '/';
             }
         }
     },
     {
-        path: '/registration', name: "Registration", component: () => import("@/components/module_reg_auth/RegistrationPage.vue"), beforeEnter: () => {
+        path: '/registration',
+        name: "Registration",
+        component: () => import("@/components/module_reg_auth/RegistrationPage.vue"),
+        beforeEnter: async () => {
             const userStore = useUserStore();
+            await userStore.loadUserData();
             if (userStore.isLogged === true) {
                 return '/';
             }
